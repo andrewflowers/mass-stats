94e47a4297f1e3cb48455d3cdac745d886e80239
andrew_flowers <aflowers@indeed.com>
2017-07-23 07:29:08 -0400

    updated map and topojson files

--- javascripts/map.js
+++ javascripts/map.js
@@ -4,7 +4,10 @@ var geo_data;
 // Map specifications
 var map_width = 1500, map_height = 800;
 
-var projection = d3.geo.statePlane("MA");
+var projection = d3.geo.mercator()
+	.center([-70, 50]) 
+	.scale([map_width])
+	.translate([map_width / 2, map_height / 2]);
 
 var path = d3.geo.path().projection(projection);
 
@@ -16,12 +19,12 @@ var svg = d3.select("body")
 
 function create_map(geo_data) {
 
-	towns = topojson.feature(geo_data, geo_data.objects.TOWNS_POLYM).features;
-
+	//towns = topojson.feature(geo_data, geo_data.objects.towns).features;
+	towns = geo_data.objects.towns;
 	console.log(towns);
 
 	svg.selectAll("path")
-		.data(towns)
+		.data(towns.features)
 		.enter()
 		.append("path")
 		.attr("d", path);
@@ -29,7 +32,7 @@ function create_map(geo_data) {
 }
 
 // Load geo data and create map
-d3.json("mass_towns.json", function(error, geo_data) {
+d3.json("../ma-towns.topojson", function(error, geo_data) {
 	
 	if (error) {
 		console.error(error);
