922efad0be2d1d5c59de8c7a2265ea99ebca2937
Andrew Flowers <andrew.w.flowers@gmail.com>
2017-07-30 12:13:36 -0400

    attempted map fix

--- javascripts/map.js
+++ javascripts/map.js
@@ -2,29 +2,30 @@
 var geo_data;
 
 // Map specifications
-var map_width = 1500, map_height = 800;
+var map_width = 700, map_height = 500;
 
 var projection = d3.geo.mercator()
-	.center([-70, 50]) 
-	.scale([map_width])
-	.translate([map_width / 2, map_height / 2]);
+	.center([71, 42]) 
+	.scale([map_width*500]);
+	//.translate([map_width / 2, map_height / 2]);
 
 var path = d3.geo.path().projection(projection);
 
 // Create map svg element
-var svg = d3.select("body")
+var map_svg = d3.select("body")
 	.append("svg")
-	.attr("width", map_width)
-	.attr("height", map_height);
+		.attr("width", map_width)
+		.attr("height", map_height)
+		.attr("id", "map_svg");
 
 function create_map(geo_data) {
 
-	//towns = topojson.feature(geo_data, geo_data.objects.towns).features;
-	towns = geo_data.objects.towns;
-	console.log(towns);
+	console.log(geo_data);
 
-	svg.selectAll("path")
-		.data(towns.features)
+	towns = topojson.feature(geo_data, geo_data.objects.TOWNS_POLYM).features;
+	
+	map_svg.selectAll("path")
+		.data(towns)
 		.enter()
 		.append("path")
 		.attr("d", path);
@@ -32,7 +33,7 @@ function create_map(geo_data) {
 }
 
 // Load geo data and create map
-d3.json("../ma-towns.topojson", function(error, geo_data) {
+d3.json("../mass_towns.json", function(error, geo_data) {
 	
 	if (error) {
 		console.error(error);
